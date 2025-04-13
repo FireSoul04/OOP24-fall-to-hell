fabfef6de51c3c697b173d215280b884cf011d39
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-04-13 11:44:12 +0200

    Moved the controller methods from game logic to game controller and added game state to the controller

--- src/main/java/it/unibo/falltohell/controller/impl/GameControllerImpl.java
+++ src/main/java/it/unibo/falltohell/controller/impl/GameControllerImpl.java
@@ -1,20 +1,33 @@
 package it.unibo.falltohell.controller.impl;
 
+import it.unibo.falltohell.controller.api.GameController;
 import it.unibo.falltohell.model.api.Game;
 import it.unibo.falltohell.model.impl.GameImpl;
 import it.unibo.falltohell.view.api.GameWindow;
 import it.unibo.falltohell.view.impl.GameWindowImpl;
 
-public class GameControllerImpl {
+public class GameControllerImpl implements GameController {
+
+    private static final double MAX_UPDATES = 60.0;
+    
+    private enum GameState {
+        RUNNING,
+        START,
+        OVER //,
+        // PAUSE
+    }
 
     private final GameWindow view;
     private final Game model;
+    private GameState state;
 
     public GameControllerImpl() {
         this.model = new GameImpl();
         this.view = new GameWindowImpl(240, 240);
+        this.state = GameState.START;
     }
 
+    @Override
     public void run() {
         final double ns = 1.0E9 / MAX_UPDATES;
         double deltaTime = 0.0;
@@ -36,7 +49,7 @@ public class GameControllerImpl {
             }
             this.render();
             frames++;
-            frameRateEndTime = System.nanoTime() - frameRateStartTime;
+            final long frameRateEndTime = System.nanoTime() - frameRateStartTime;
             if (frameRateEndTime > 1.0) {
                 System.out.println(updates + " ups, " + frames + " fps");
                 updates = 0;
@@ -44,5 +57,25 @@ public class GameControllerImpl {
                 frameRateStartTime = System.nanoTime();
             }
         }
+    }
+
+    @Override
+    public boolean isOver() {
+        return this.state == GameState.OVER;
+    }
+
+    @Override
+    public boolean isRunning() {
+        return this.state == GameState.RUNNING;
+    }
+
+    @Override
+    public void update(final double deltaTime) {
+
+    }
+
+    @Override
+    public void render() {
+
     }
 }
--- src/main/java/it/unibo/falltohell/model/impl/GameImpl.java
+++ src/main/java/it/unibo/falltohell/model/impl/GameImpl.java
@@ -4,36 +4,8 @@ import it.unibo.falltohell.model.api.Game;
 
 public class GameImpl implements Game {
 
-    private static final double MAX_UPDATES = 60.0;
-    
-    private enum GameState {
-        RUNNING,
-        START,
-        OVER //,
-        // PAUSE
-    }
-
-    public GameImpl() {
-        
-    }
-
+    @Override
     public void init() {
-
-    }
-
-    public boolean isOver() {
-        return false;
-    }
-
-    public boolean isRunning() {
-        return false;
-    }
-
-    public void update(final double deltaTime) {
-
-    }
-
-    public void render() {
-
+        
     }
 }
