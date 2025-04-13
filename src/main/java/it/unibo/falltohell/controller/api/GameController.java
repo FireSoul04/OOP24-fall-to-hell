870f7feea11165c526d913901579dac66bce7553
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-04-13 11:44:12 +0200

    Moved the controller methods from game logic to game controller and added game state to the controller

--- src/main/java/it/unibo/falltohell/controller/api/GameController.java
+++ src/main/java/it/unibo/falltohell/controller/api/GameController.java
@@ -6,4 +6,25 @@ public interface GameController {
      * Game loop, runs at MAX_UPDATES per seconds and handles the rendering and lets the game work on multiple platforms at the same speed.
      */
     void run();
+
+    /**
+     * @return true when the game is over, false otherwise
+     */
+    boolean isOver();
+
+    /**
+     * @return true when the game is running, false otherwise
+     */
+    boolean isRunning();
+
+    /**
+     * Update all the game objects inside the current level/scene.
+     * @param deltaTime time passed between the last frame and the current frame
+     */
+    void update(double deltaTime);
+
+    /**
+     * Tell the controller to update the game window (view).
+     */
+    void render();
 }
--- src/main/java/it/unibo/falltohell/model/api/Movable.java
+++ src/main/java/it/unibo/falltohell/model/api/Movable.java
@@ -1,7 +1,5 @@
 package it.unibo.falltohell.model.api;
 
-import it.unibo.falltohell.model.api.GameObject;
-
 public interface Movable extends GameObject {
     
     /**
