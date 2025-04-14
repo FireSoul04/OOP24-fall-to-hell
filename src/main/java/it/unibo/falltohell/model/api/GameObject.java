c12d4ed42679d5695716faed6392c9cd2fb68e56
conno711 <lorenzo.casadei16@studio.unibo.it>
2025-04-14 10:47:32 +0200

    Aggiunta dell'interfaccia GameObject e implementazione base

--- src/main/java/it/unibo/falltohell/model/api/GameObject.java
+++ src/main/java/it/unibo/falltohell/model/api/GameObject.java
@@ -8,4 +8,29 @@ public interface GameObject {
      * @return current position of this game object
      */
     Position getPosition();
+    
+    /**
+     * @return the width of the game object
+     */
+    double getWidth();
+
+    /**
+     * @return the height of the game object
+     */
+    double getHeight();
+
+    /**
+     * @return true if the game object is solid, false otherwise
+     */
+    boolean isSolid();
+
+    /**
+     * @return the width of the game object in terms of tile size
+     */
+    double getWidthSize();
+
+    /**
+     * @return the height of the game object in terms of tile size
+     */
+    double getHeightSize();
 }
