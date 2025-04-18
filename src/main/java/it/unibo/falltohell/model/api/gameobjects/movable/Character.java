872dc79a0c2340e33b0f8f8966e86d74c9a8e041
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-04-18 11:32:00 +0200

    Added skeleton of buff decorator

--- src/main/java/it/unibo/falltohell/model/api/gameobjects/movable/Character.java
+++ src/main/java/it/unibo/falltohell/model/api/gameobjects/movable/Character.java
@@ -3,7 +3,7 @@ package it.unibo.falltohell.model.api.gameobjects.movable;
 import it.unibo.falltohell.model.api.gameobjects.Interactable;
 import it.unibo.falltohell.model.api.gameobjects.Movable;
 
-public interface Player extends Movable {
+public interface Character extends Movable {
     
     /**
      * @param interactable object to be interacted with
