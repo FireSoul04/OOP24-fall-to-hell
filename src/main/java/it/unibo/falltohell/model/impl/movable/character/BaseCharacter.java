0b82decfdbe2e0aa02112ef63fde249b80cf99fc
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-05-01 12:43:24 +0200

    Made BaseCharacter inherit Movable and removed methods already defined in super class

--- src/main/java/it/unibo/falltohell/model/impl/movable/character/BaseCharacter.java
+++ src/main/java/it/unibo/falltohell/model/impl/movable/character/BaseCharacter.java
@@ -1,63 +1,18 @@
 package it.unibo.falltohell.model.impl.movable.character;
 
+import it.unibo.falltohell.model.impl.MovableImpl;
 import it.unibo.falltohell.model.util.Vector2;
 import it.unibo.falltohell.model.api.gameobjects.Interactable;
 import it.unibo.falltohell.model.api.gameobjects.movable.Character;
 
-// TODO remove the methods that needs to be inherited by movable when the interface gets an implementation
+public class BaseCharacter extends MovableImpl implements Character {
 
-public class BaseCharacter implements Character {
-
-    public BaseCharacter() {
-
+    public BaseCharacter(Vector2 position) {
+        super(position, 20, 20, 20, 20);
     }
 
     @Override
-    public void move(final double deltaTime) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'move'");
-    }
+    public void interact(Interactable interactable) {
 
-    @Override
-    public Vector2 getPosition() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
     }
-
-    @Override
-    public double getWidth() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getWidth'");
-    }
-
-    @Override
-    public double getHeight() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getHeight'");
-    }
-
-    @Override
-    public boolean isSolid() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isSolid'");
-    }
-
-    @Override
-    public double getWidthSize() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getWidthSize'");
-    }
-
-    @Override
-    public double getHeightSize() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getHeightSize'");
-    }
-
-    @Override
-    public void interact(final Interactable interactable) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'interact'");
-    }
-
 }
