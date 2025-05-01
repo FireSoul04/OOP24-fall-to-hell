963201ea30399af1e8c0b39264535db3190f4879
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-05-01 16:02:45 +0200

    Added Collider to GameObject and its implementation

--- src/main/java/it/unibo/falltohell/model/impl/GameObjectImpl.java
+++ src/main/java/it/unibo/falltohell/model/impl/GameObjectImpl.java
@@ -1,35 +1,45 @@
 package it.unibo.falltohell.model.impl;
 
+import it.unibo.falltohell.model.api.Collider;
 import it.unibo.falltohell.model.api.GameObject;
 import it.unibo.falltohell.model.util.Vector2;
 
 public class GameObjectImpl implements GameObject {
     private Vector2 pos;
+    private Collider collider;
     private double width;
     private double height;
     private boolean isSolid;
     private double widthSize;
     private double heightSize;
 
-    public GameObjectImpl(Vector2 vector2, double width, double height) {
-        this.pos = vector2;
+    public GameObjectImpl(Vector2 position, double width, double height, Collider collider) {
+        this.pos = position;
         this.width = width;
         this.height = height;
         this.isSolid = true; // Default
         this.widthSize = width * GameObject.TILE_SIZE;
         this.heightSize = height * GameObject.TILE_SIZE;
+        this.collider = collider;
     }
-    public GameObjectImpl(Vector2 vector2, double width, double height, boolean isSolid) {
-        this.pos = vector2;
+    public GameObjectImpl(Vector2 position, double width, double height, boolean isSolid, Collider collider, {
+        this.pos = position;
         this.width = width;
         this.height = height;
         this.isSolid = isSolid; 
         this.widthSize = width * GameObject.TILE_SIZE;
         this.heightSize = height * GameObject.TILE_SIZE;
+        this.collider = collider;
     }
     public Vector2 getPosition(){
         return this.pos;
     }
+
+    @Override
+    public Collider getCollider() {
+        return this.collider;
+    }
+
     public double getWidth(){
         return this.width;
     }
@@ -47,8 +57,8 @@ public class GameObjectImpl implements GameObject {
         return this.heightSize;
     }
     
-    public void setPosition(Vector2 vector2) {
-        this.pos = vector2;
+    public void setPosition(Vector2 position) {
+        this.pos = position;
     }
     
     public void onCollide(GameObject other) {
