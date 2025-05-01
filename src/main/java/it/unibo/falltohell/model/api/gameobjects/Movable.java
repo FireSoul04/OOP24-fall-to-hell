3e37cd1478405ce55f3aef261a71b33e135c6bcc
Lorenzo Casadei <lorenzo.casadei16@studio.unibo.it>
2025-05-01 11:23:41 +0200

    First implementation of movable and projectile, to change

--- src/main/java/it/unibo/falltohell/model/api/gameobjects/Movable.java
+++ src/main/java/it/unibo/falltohell/model/api/gameobjects/Movable.java
@@ -8,4 +8,12 @@ public interface Movable extends GameObject {
      * @param deltaTime elapsed time between the current frame and the last one
      */
     void move(double deltaTime);
+
+    double getSpeedX();
+    
+    double getSpeedY();
+
+    void setSpeedX(double speedX);
+    
+    void setSpeedY(double speedY);
 }
