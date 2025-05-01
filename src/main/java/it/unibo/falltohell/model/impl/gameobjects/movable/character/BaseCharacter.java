06a94671e0e01a30a7893c09490125017d060d8f
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-05-01 12:45:29 +0200

    Moved movable packate to gameobjects package inside implementation

--- src/main/java/it/unibo/falltohell/model/impl/gameobjects/movable/character/BaseCharacter.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/movable/character/BaseCharacter.java
@@ -1,4 +1,4 @@
-package it.unibo.falltohell.model.impl.movable.character;
+package it.unibo.falltohell.model.impl.gameobjects.movable.character;
 
 import it.unibo.falltohell.model.api.Position;
 import it.unibo.falltohell.model.api.gameobjects.Interactable;
