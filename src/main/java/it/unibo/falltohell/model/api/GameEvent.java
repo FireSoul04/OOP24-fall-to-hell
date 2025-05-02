f7ba7cf9bf8bb83629c02d91347737578a510cdb
conno711 <lorenzo.casadei16@studio.unibo.it>
2025-05-02 10:44:59 +0200

    added GameEvent interface and implementation of Level

--- src/main/java/it/unibo/falltohell/model/api/GameEvent.java
+++ src/main/java/it/unibo/falltohell/model/api/GameEvent.java
@@ -1,5 +1,21 @@
 package it.unibo.falltohell.model.api;
 
 public interface GameEvent {
+//    public enum EventType {
+//         PLAYER_HIT,
+//         ENEMY_HIT,
+//         PROJECTILE_HIT,
+//         PLAYER_DEATH,
+//         ENEMY_DEATH,
+//         COLLISION,
+//         POWER_UP_ACTIVATED,
+//         GAME_OVER
+//     }
 
+//     EventType getEventType();
+
+//     GameObject getGameObject();
+      void execute();
 }
+
+    
