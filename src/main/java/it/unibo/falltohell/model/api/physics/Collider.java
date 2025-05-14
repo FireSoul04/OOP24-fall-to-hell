236fd65529725883c75ca2bdc72858fcf771ec92
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-05-14 22:14:27 +0200

    Added class description to Collider and CollisionsManager

--- src/main/java/it/unibo/falltohell/model/api/physics/Collider.java
+++ src/main/java/it/unibo/falltohell/model/api/physics/Collider.java
@@ -2,6 +2,11 @@ package it.unibo.falltohell.model.api.physics;
 
 import it.unibo.falltohell.model.util.Vector2;
 
+/**
+ * Class for a generic collider.
+ *
+ * @author Davide Mancini
+ */
 public interface Collider {
 
     /**
