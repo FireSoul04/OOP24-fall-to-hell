8bf624b401c524527227bdbef5ba24aeac516e71
Sara Visani <sara.visani7@studio.unibo.it>
2025-05-14 14:04:02 +0200

    implemented Enemy Factory and initialized different Monster classes

--- src/main/java/it/unibo/falltohell/model/impl/EnemyFactoryImpl.java
+++ src/main/java/it/unibo/falltohell/model/impl/EnemyFactoryImpl.java
@@ -2,20 +2,20 @@ package it.unibo.falltohell.model.impl;
 
 import it.unibo.falltohell.model.api.EnemyFactory;
 import it.unibo.falltohell.model.api.gameobjects.movable.entity.Enemy;
+import it.unibo.falltohell.model.impl.gameobjects.movable.character.Entity.Enemy.Monster1;
+import it.unibo.falltohell.model.impl.gameobjects.movable.character.Entity.Enemy.Monster2;
 import it.unibo.falltohell.model.util.Vector2;
 
 public class EnemyFactoryImpl implements EnemyFactory{
 
     @Override
     public Enemy CreateMonster1(Vector2 initialCords) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'CreateMonster1'");
+        return new Monster1(initialCords);
     }
 
     @Override
     public Enemy CreateMonster2(Vector2 initialCords) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'CreateMonster2'");
+        return new Monster2(initialCords);
     }
     
 }
