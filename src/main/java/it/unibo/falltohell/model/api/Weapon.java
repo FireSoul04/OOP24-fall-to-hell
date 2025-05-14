1bbef58f453d3a311c293f58c8d5e6806ae19574
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-05-14 22:08:37 +0200

    Added skeleton of weapon strategy

---
+++ src/main/java/it/unibo/falltohell/model/api/Weapon.java
@@ -0,0 +1,14 @@
+package it.unibo.falltohell.model.api;
+
+/**
+ * Interface for any weapon in the game.
+ *
+ * @author Davide Mancini
+ */
+public interface Weapon {
+
+	/**
+	 * Perform an attack.
+	 */
+	void attack();
+}
---
+++ src/main/java/it/unibo/falltohell/model/impl/weapons/RangedWeapon.java
@@ -0,0 +1,17 @@
+package it.unibo.falltohell.model.impl.weapons;
+
+import it.unibo.falltohell.model.api.Weapon;
+
+/**
+ * Weapon that shoots projectiles from afar.
+ * This weapon doesn't collide.
+ *
+ * @author Davide Mancini
+ */
+public abstract class RangedWeapon implements Weapon {
+
+	@Override
+	public void attack() {
+		// TODO add a method to create a projectile
+	}
+}
