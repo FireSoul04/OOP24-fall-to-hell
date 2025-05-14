62d589d76a0d72407b6e16cc264209a26e3d0fb8
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-05-14 22:08:37 +0200

    Added skeleton of weapon strategy

---
+++ src/main/java/it/unibo/falltohell/model/impl/weapons/MeleeWeapon.java
@@ -0,0 +1,35 @@
+package it.unibo.falltohell.model.impl.weapons;
+
+import it.unibo.falltohell.model.api.Collider;
+import it.unibo.falltohell.model.api.Weapon;
+
+/**
+ * Weapon that attack on close range.
+ * This weapon uses a collider to check for any hits.
+ *
+ * @author Davide Mancini
+ */
+public abstract class MeleeWeapon implements Weapon {
+
+	private final Collider collider;
+
+	/**
+	 * Creates an abstract close ranged weapon.
+	 * @param collider representing the hitbox of the weapon when it attacks
+	 */
+	public MeleeWeapon(final Collider collider) {
+		this.collider = collider;
+	}
+
+	@Override
+	public void attack() {
+		// TODO add a timer to determine when the weapon can check for any hits
+	}
+
+	/**
+	 * @return hitbox collider
+	 */
+	protected Collider getCollider() {
+		return this.collider;
+	}
+}
