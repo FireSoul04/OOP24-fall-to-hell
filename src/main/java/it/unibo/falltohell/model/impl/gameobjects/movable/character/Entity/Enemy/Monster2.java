669217014570c3ee2a21efa8331325b00a561bf1
Sara Visani <sara.visani7@studio.unibo.it>
2025-05-14 14:04:02 +0200

    implemented Enemy Factory and initialized different Monster classes

---
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/movable/character/Entity/Enemy/Monster2.java
@@ -0,0 +1,98 @@
+package it.unibo.falltohell.model.impl.gameobjects.movable.character.Entity.Enemy;
+
+import it.unibo.falltohell.model.api.Collider;
+import it.unibo.falltohell.model.api.GameObject;
+import it.unibo.falltohell.model.api.gameobjects.movable.entity.Enemy;
+import it.unibo.falltohell.model.util.Vector2;
+
+public class Monster2 extends Enemy{
+
+    public Monster2(Vector2 initialCords) {
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void update(double deltaTime) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public double getSpeedX() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSpeedX'");
+    }
+
+    @Override
+    public double getSpeedY() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSpeedY'");
+    }
+
+    @Override
+    public void setSpeedX(double speedX) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setSpeedX'");
+    }
+
+    @Override
+    public void setSpeedY(double speedY) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setSpeedY'");
+    }
+
+    @Override
+    public Vector2 getPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+
+    @Override
+    public Collider getCollider() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCollider'");
+    }
+
+    @Override
+    public double getWidth() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getWidth'");
+    }
+
+    @Override
+    public double getHeight() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHeight'");
+    }
+
+    @Override
+    public boolean isSolid() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isSolid'");
+    }
+
+    @Override
+    public double getWidthSize() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getWidthSize'");
+    }
+
+    @Override
+    public double getHeightSize() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHeightSize'");
+    }
+
+    @Override
+    public void setPosition(Vector2 vector2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPosition'");
+    }
+
+    @Override
+    public void onCollide(GameObject other) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onCollide'");
+    }
+    
+}
