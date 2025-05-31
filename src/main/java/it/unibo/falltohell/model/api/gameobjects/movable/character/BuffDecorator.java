fa2571f41379853ddced417d46d44871456d0089
Sara Visani <sara.visani7@studio.unibo.it>
2025-05-31 18:57:27 +0200

    Deleted duplicates

--- src/main/java/it/unibo/falltohell/model/api/gameobjects/movable/character/BuffDecorator.java
+++
@@ -1,7 +0,0 @@
-package it.unibo.falltohell.model.api.gameobjects.movable.character;
-
-import it.unibo.falltohell.model.api.gameobjects.movable.entity.Character;
-
-public interface BuffDecorator extends Character {
-
-}
--- src/main/java/it/unibo/falltohell/model/api/gameobjects/movable/character/RangedWeapon.java
+++
@@ -1,5 +0,0 @@
-package it.unibo.falltohell.model.api.gameobjects.movable.character;
-
-public interface RangedWeapon {
-
-}
