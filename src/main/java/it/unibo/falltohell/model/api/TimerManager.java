300c73a68d7d755c08149a2c5a1ba07a97665f51
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-06-16 11:36:57 +0200

    Added the TimerManager interface

---
+++ src/main/java/it/unibo/falltohell/model/api/TimerManager.java
@@ -0,0 +1,44 @@
+package it.unibo.falltohell.model.api;
+
+/**
+ * Interface to handle multiple timers
+ * @author Martina Malagoli
+ */
+public interface TimerManager {
+
+    /**
+     * Method to add and start a new timer to the TimerManager
+     * @param name of the new timer
+     * @param timer to be added
+     */
+    void addTimer(String name, CustomTimer timer);
+
+    /**
+     * Method to remove and stop a timer from the TimerManager
+     * @param name of the timer to be removed
+     */
+    void removeTimer(String name);
+
+    /**
+     * Method to pause a specific timer
+     * @param name of the timer to be paused
+     */
+    void pauseTimer(String name);
+
+    /**
+     * Method to pause all timers
+     */
+    void pauseAllTimers();
+
+    /**
+     * Method to resume a specific timer
+     * @param name of the timer to be resumed
+     */
+    void resumeTimer(String name);
+
+    /**
+     * Method to resume all timers
+     */
+    void resumeAllTimers();
+
+}
