5eeff9f780b3aae1001a4d2231465d638c3bfdad
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-06-16 13:51:55 +0200

    Added periods to javadoc comments

--- src/main/java/it/unibo/falltohell/model/api/CustomTimer.java
+++ src/main/java/it/unibo/falltohell/model/api/CustomTimer.java
@@ -1,40 +1,40 @@
 package it.unibo.falltohell.model.api;
 
 /**
- * Customized timer to schedule game events
+ * Customized timer to schedule game events.
  * @author Martina Malagoli
  */
 public interface CustomTimer {
 
     /**
-     * Method to start the timer
+     * Method to start the timer.
      */
     void start();
 
     /**
-     * Method to check if the timer is actually running
+     * Method to check if the timer is actually running.
      * @return if the timer is running
      */
     boolean isStarted();
 
     /**
-     * Method to check if the timer is paused
+     * Method to check if the timer is paused.
      * @return if the timer is paused
      */
     boolean isPaused();
 
     /**
-     * Method to end the timer
+     * Method to end the timer.
      */
     void stop();
 
     /**
-     * Method to pause the timer
+     * Method to pause the timer.
      */
     void pause();
 
     /**
-     * Method to resume the game
+     * Method to resume the timer.
      */
     void resume();
 }
