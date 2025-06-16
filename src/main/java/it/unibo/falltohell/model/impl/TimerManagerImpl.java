d1c1fc16080652fd0099fb6aba7ebbad14e2ae2c
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-06-16 14:00:43 +0200

    Added periods to javadoc comments, added final in some parameters and added inheritedDoc to the methods

--- src/main/java/it/unibo/falltohell/model/impl/TimerManagerImpl.java
+++ src/main/java/it/unibo/falltohell/model/impl/TimerManagerImpl.java
@@ -7,35 +7,51 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Class to handle multiple timers
+ * Class to handle multiple timers.
  * @author Martina Malagoli
  */
 public class TimerManagerImpl implements TimerManager {
+
     private final Map<String, CustomTimer> timers;
     //private boolean allPaused; TODO --> VERIFY IF NEEDED
 
+    /**
+     * Initialization of the TimeManager.
+     */
     public TimerManagerImpl() {
         this.timers = new HashMap<>();
         //this.allPaused = false;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void addTimer(String name, CustomTimer timer) {
+    public void addTimer(final String name, final CustomTimer timer) {
         this.timers.put(name, timer);
         timer.start();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void removeTimer(String name) {
+    public void removeTimer(final String name) {
         this.timers.get(name).stop();
         this.timers.remove(name);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void pauseTimer(String name) {
+    public void pauseTimer(final String name) {
         this.timers.get(name).pause();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void pauseAllTimers() {
         for (final CustomTimer timer : this.timers.values()) {
@@ -45,11 +61,17 @@ public class TimerManagerImpl implements TimerManager {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void resumeTimer(String name) {
+    public void resumeTimer(final String name) {
         this.timers.get(name).resume();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void resumeAllTimers() {
         for (final CustomTimer timer : this.timers.values()) {
