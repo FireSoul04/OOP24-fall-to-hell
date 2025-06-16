1b149fb23ff7514c91c80c30d43c636902ed18d9
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-06-16 12:52:16 +0200

    Added javacod describing collision class

--- src/main/java/it/unibo/falltohell/model/impl/physics/colliders/Collision.java
+++ src/main/java/it/unibo/falltohell/model/impl/physics/colliders/Collision.java
@@ -2,5 +2,12 @@ package it.unibo.falltohell.model.impl.physics.colliders;
 
 import it.unibo.falltohell.model.util.Vector2;
 
+/**
+ * Class that determine a collision.
+ *
+ * @author Davide Mancini
+ *
+ * @param direction where the collision happened
+ */
 public record Collision(Vector2 direction) {
 }
