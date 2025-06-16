1dab3049c9a99fefb28c1f1b45cbdb572c51cf91
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-06-16 12:56:14 +0200

    Added inheritDoc for method checkCollisions and changed check for two game objects using object equals method

--- src/main/java/it/unibo/falltohell/model/impl/physics/colliders/AbstractCollisionsManager.java
+++ src/main/java/it/unibo/falltohell/model/impl/physics/colliders/AbstractCollisionsManager.java
@@ -13,11 +13,14 @@ import java.util.Optional;
  */
 public abstract class AbstractCollisionsManager implements CollisionsManager {
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void checkCollisions(final List<GameObject> gameObjects) {
         for (final GameObject g1 : gameObjects) {
             for (final GameObject g2 : gameObjects) {
-                if (g1 != g2 && g1.isSolid() && g2.isSolid()) {
+                if (!g1.equals(g2) && g1.isSolid() && g2.isSolid()) {
                     final Optional<Collision> collision = this.determineCollision(g1, g2);
 
                     if (collision.isPresent()) {
