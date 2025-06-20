bbd5014c8793a9343bacedb1372599717cad9f27
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-06-20 13:17:00 +0200

    Added enum for identifying a character type

--- src/main/java/it/unibo/falltohell/model/api/gameobjects/movable/entity/Character.java
+++ src/main/java/it/unibo/falltohell/model/api/gameobjects/movable/entity/Character.java
@@ -4,9 +4,25 @@ import it.unibo.falltohell.model.api.gameobjects.Interactable;
 import it.unibo.falltohell.model.api.gameobjects.movable.Entity;
 
 public interface Character extends Entity {
+
+    /**
+     * Identifier for the type of character.
+     */
+    enum CharacterID {
+        ROGUE,
+        CASTER,
+        ARCHER,
+        DRUID,
+        FIGHTER
+    }
     
     /**
      * @param interactable object to be interacted with
      */
     void interact(Interactable interactable);
+
+    /**
+     * @return this character id
+     */
+    CharacterID getCharacterID();
 }
