d29fe0e3d02f4fc02661d8ec4276d071548254b4
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-06-20 17:20:13 +0200

    Added the method save and added some javadoc

--- src/main/java/it/unibo/falltohell/model/api/gameobjects/interactable/SavePoint.java
+++ src/main/java/it/unibo/falltohell/model/api/gameobjects/interactable/SavePoint.java
@@ -2,6 +2,17 @@ package it.unibo.falltohell.model.api.gameobjects.interactable;
 
 import it.unibo.falltohell.model.api.gameobjects.Interactable;
 
+/**
+ * Game object that the player can interact with to save the
+ * current state of the game or to change the current character.
+ * @author Martina Malagoli
+ */
 public interface SavePoint extends Interactable {
+
+    /**
+     * Method to save the game data.
+     */
+    void save();
+
 
 }
