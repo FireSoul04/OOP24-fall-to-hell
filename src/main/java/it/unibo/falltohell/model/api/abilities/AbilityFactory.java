5e5dde73cc40ff94fce1c2fcdaa028152c206cd3
Sara Visani <sara.visani7@studio.unibo.it>
2025-06-21 20:16:39 +0200

    Initial implementation for ability and ability factory

---
+++ src/main/java/it/unibo/falltohell/model/api/abilities/AbilityFactory.java
@@ -0,0 +1,13 @@
+package it.unibo.falltohell.model.api.abilities;
+
+/**
+ * Implements abstract class Enemies, creates second type for enemies
+ * @author Sara Visani
+ */
+
+public interface AbilityFactory {
+    
+    Ability createActiveAbility();
+
+    Ability createPassiveAbility();
+}
---
+++ src/main/java/it/unibo/falltohell/model/api/abilities/ActiveAbility.java
@@ -0,0 +1,10 @@
+package it.unibo.falltohell.model.api.abilities;
+
+/**
+ * Implements abstract class Enemies, creates second type for enemies
+ * @author Sara Visani
+ */
+
+public interface ActiveAbility extends Ability{
+    
+}
