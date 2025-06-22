27fbff66813580aa4ece43a035723de830c355e8
Sara Visani <sara.visani7@studio.unibo.it>
2025-06-22 12:19:04 +0200

    Added forgotten extention

--- src/main/java/it/unibo/falltohell/model/api/abilities/ActiveAbility.java
+++ src/main/java/it/unibo/falltohell/model/api/abilities/ActiveAbility.java
@@ -1,10 +1,12 @@
 package it.unibo.falltohell.model.api.abilities;
 
+import it.unibo.falltohell.model.api.gameobjects.Movable;
+
 /**
  * Interface for Active type of abilities
  * @author Sara Visani
  */
 
-public interface ActiveAbility extends Ability{
+public interface ActiveAbility extends Ability, Movable{
     
 }
