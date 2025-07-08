314b33fa64bb383a07b989fa999e30d94c0e67be
Sara Visani <sara.visani7@studio.unibo.it>
2025-07-08 18:45:33 +0200

    Updated comments

--- src/main/java/it/unibo/falltohell/model/api/EnemyFactory.java
+++ src/main/java/it/unibo/falltohell/model/api/EnemyFactory.java
@@ -5,26 +5,37 @@ import it.unibo.falltohell.model.api.gameobjects.movable.entity.Enemy;
 import it.unibo.falltohell.model.util.Vector2;
 
 /**
- * Interface to ask creations of a specific type of enemy.
+ * Factory interface for creating different types of {@link Enemy} instances in
+ * a level.
+ * This interface abstracts the logic for instantiating specific enemy types
+ * tied to
+ * a given {@link Level} and a {@link Character}.
+ * <p>
+ * It helps decouple the creation logic of enemies from the game engine.
  *
  * @author Sara Visani
  */
-
 public interface EnemyFactory {
 
     /**
-     * @param level        level where the enemy is
-     * @param initialCords where the monster is created
-     * @param character    the player character in the level
-     * @return the monster type 1
+     * Creates an instance of monster type 1.
+     * <p>
+     *
+     * @param level        the {@link Level} in which the enemy is spawned
+     * @param initialCords the {@link Vector2} position where the enemy is created
+     * @param character    the {@link Character} the enemy will interact with
+     * @return a new instance of monster type 1 as an {@link Enemy}
      */
     Enemy createMonster1(Level level, Vector2 initialCords, Character character);
 
     /**
-     * @param level        level where the enemy is
-     * @param initialCords where the monster is created
-     * @param character    the player character in the level
-     * @return the monster type 2
+     * Creates an instance of monster type 2.
+     * <p>
+     *
+     * @param level        the {@link Level} in which the enemy is spawned
+     * @param initialCords the {@link Vector2} position where the enemy is created
+     * @param character    the {@link Character} the enemy will interact with
+     * @return a new instance of monster type 2 as an {@link Enemy}
      */
     Enemy createMonster2(Level level, Vector2 initialCords, Character character);
 }
--- src/main/java/it/unibo/falltohell/model/impl/EnemyFactoryImpl.java
+++ src/main/java/it/unibo/falltohell/model/impl/EnemyFactoryImpl.java
@@ -9,7 +9,17 @@ import it.unibo.falltohell.model.util.Vector2;
 import it.unibo.falltohell.model.api.gameobjects.movable.entity.Character;
 
 /**
- * Implements abstract factory for Enemies, creates Enemies.
+ * Concrete implementation of the {@link EnemyFactory} interface.
+ * <p>
+ * This class is responsible for creating specific {@link Enemy} instances,
+ * such as {@link Monster1} and {@link Monster2}.
+ * </p>
+ *
+ * @see Enemy
+ * @see Monster1
+ * @see Monster2
+ * @see Level
+ * @see Character
  *
  * @author Sara Visani
  */
