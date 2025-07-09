deb66fef0720b5f4b3f068d602280ee4a9aebb8d
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-09 12:42:25 +0200

    Added methods and some javadoc to the interface

--- src/main/java/it/unibo/falltohell/view/api/Renderable.java
+++ src/main/java/it/unibo/falltohell/view/api/Renderable.java
@@ -1,12 +1,38 @@
 package it.unibo.falltohell.view.api;
 
-import java.awt.*;
+import it.unibo.falltohell.model.util.Vector2;
 
+/**
+ * Interface that represents objects that can be rendered.
+ * @author Martina Malagoli
+ */
 public interface Renderable {
 
     /**
-     *
-     * @return the image to be rendered
+     * Method to mirror the image associated to the renderable object.
+     * @param mirroring tells if the image associated to the renderable object should be mirrored
      */
-    Image getImage();
+    void mirror(boolean mirroring);
+
+    /**
+     * @return if the image associated to the renderable object is visible
+     */
+    boolean isVisible();
+
+    /**
+     * @param visibility tells if the image associated to the renderable object should be visible
+     */
+    void setVisibility(boolean visibility);
+
+    /**
+     * @return the current position of the image associated to the renderable object
+     */
+    Vector2 getPosition();
+
+    /**
+     * Method to change the current position of the image associated to the renderable object.
+     * @param newPosition is the new position of the image associated to the renderable object
+     */
+    void translate(Vector2 newPosition);
+
 }
