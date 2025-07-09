b627e65f0d74917d510b2e2528d210c6efe1fb9d
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-09 19:12:25 +0200

    Removed the Buff and BuffDecorator interfaces

--- src/main/java/it/unibo/falltohell/model/api/gameobjects/movable/entity/weapons/BuffDecorator.java
+++
@@ -1,7 +0,0 @@
-package it.unibo.falltohell.model.api.gameobjects.movable.entity.weapons;
-
-import it.unibo.falltohell.model.api.gameobjects.movable.entity.Character;
-
-public interface BuffDecorator extends Character {
-
-}
