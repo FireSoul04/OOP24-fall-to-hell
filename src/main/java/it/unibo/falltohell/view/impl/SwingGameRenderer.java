7104a88e671c244515f5d41c5dfb9ce07e9dd118
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-07-09 19:43:14 +0200

    Fixed getScale method in SwingGameRenderer

--- src/main/java/it/unibo/falltohell/view/impl/SwingGameRenderer.java
+++ src/main/java/it/unibo/falltohell/view/impl/SwingGameRenderer.java
@@ -50,7 +50,7 @@ public class SwingGameRenderer extends JPanel implements GameRenderer {
 		final Graphics2D g2 = (Graphics2D) g;
 		g2.setColor(Color.BLACK);
 		g2.fillRect(0, 0, this.getWidth(), this.getHeight());
-		g2.scale(this.window.getScaleX(), this.window.getScaleY());
+		g2.scale(this.window.getScale().x(), this.window.getScale().y());
 
 		// Two rectangles are drawn for debugging purpose only
 		g2.setColor(Color.WHITE);
