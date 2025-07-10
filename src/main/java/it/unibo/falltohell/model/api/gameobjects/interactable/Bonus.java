d0d22bfcc22dd1f25b531f5e4435c2bf42ffc616
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-10 22:28:42 +0200

    Deleted the interactable directory and its content

--- src/main/java/it/unibo/falltohell/model/api/gameobjects/interactable/Bonus.java
+++
@@ -1,7 +0,0 @@
-package it.unibo.falltohell.model.api.gameobjects.interactable;
-
-import it.unibo.falltohell.model.api.gameobjects.Interactable;
-
-public interface Bonus extends Interactable {
-
-}
