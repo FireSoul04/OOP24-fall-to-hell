925b718e6e0002ce369d5cdaf6c358ac8b5b96d8
Sara Visani <sara.visani7@studio.unibo.it>
2025-07-11 16:51:04 +0200

    Created this Manager to have managment of all timers of enemies without comflicts

---
+++ src/main/java/it/unibo/falltohell/model/api/gameobjects/movable/entity/EnemyTimerManager.java
@@ -0,0 +1,52 @@
+package it.unibo.falltohell.model.api.gameobjects.movable.entity;
+
+import it.unibo.falltohell.model.api.Level;
+
+/**
+ * Interface for managing enemy timer counters and their unique timer names.
+ * <p>
+ * Responsible for generating unique timer names for enemies and
+ * managing the removal of timers when enemies are removed from the level.
+ * </p>
+ *
+ * @author Sara Visani
+ */
+public interface EnemyTimerManager {
+
+    /**
+     * Creates and registers a "NoAggro" timer for the specified enemy within the given level.
+     * <p>
+     * The timer is used to handle regeneration or related behaviors when the enemy is not aggressive.
+     * </p>
+     *
+     * @param level    the level to which the enemy belongs, used to access the TimerManager
+     * @param enemy    the enemy instance for which the timer is created
+     * @param duration the duration in milliseconds of the timer
+     */
+    void createNoAggroTimer(Level level, Enemy enemy, long duration);
+
+    /**
+     * Generates and registers a unique name for an enemy's "Attack" timer.
+     *
+     * @param enemy the enemy instance
+     * @return a unique name for the "Attack" timer
+     */
+    String getNextAttackName(Enemy enemy);
+
+    /**
+     * Removes all timers associated with the given enemy from the level's TimerManager.
+     *
+     * @param enemy the enemy whose timers should be removed
+     * @param level the level to access the TimerManager
+     */
+    void removeTimersFor(Enemy enemy, Level level);
+
+    /**
+     * Returns the "NoAggro" timer name associated with the given enemy.
+     *
+     * @param enemy the enemy instance to query
+     * @return the unique timer name for the no-aggro timer
+     * @throws IllegalStateException if no timer is found for the enemy
+     */
+    public String getNoAggroTimerName(Enemy enemy);
+}
