b443746ef37a195c2aabbbcbae471e37fc3ef215
Sara Visani <sara.visani7@studio.unibo.it>
2025-07-14 14:48:02 +0200

    Fixed comments

--- src/main/java/it/unibo/falltohell/model/impl/gameobjects/movable/entity/statistics/buff/DropImpl.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/movable/entity/statistics/buff/DropImpl.java
@@ -15,11 +15,15 @@ import it.unibo.falltohell.util.Dimensions;
 import it.unibo.falltohell.util.Vector2;
 
 /**
- * Implementation of a {@link Drop} object that gives a {@link Buff} to a {@link Character}
- * upon collision and disappears either after a fixed time or after being collected.
+ * Implementation of a {@link Drop} object that gives a {@link Buff} to a
+ * {@link Character}
+ * upon collision and disappears either after a fixed time or after being
+ * collected.
  *
- * <p>This drop moves horizontally until it hits a {@link Block} from above,
- * in which case it stops moving horizontally.</p>
+ * <p>
+ * This drop moves horizontally until it hits a {@link Block} from above,
+ * in which case it stops moving horizontally.
+ * </p>
  *
  * @see Drop
  * @see Buff
@@ -37,12 +41,14 @@ public class DropImpl extends MovableImpl implements Drop {
     private final Buff buff;
 
     /**
-     * Constructs a new drop object that carries a {@link Buff}, is placed at the given position,
+     * Constructs a new drop object that carries a {@link Buff}, is placed at the
+     * given position,
      * and is scheduled to be removed after 5 seconds if not collected.
      *
-     * @param lv the {@link Level} in which the drop exists
+     * @param lv       the {@link Level} in which the drop exists
      * @param position the starting {@link Vector2} position of the drop
-     * @param buff the {@link Buff} to be applied when collected by a {@link Character}
+     * @param buff     the {@link Buff} to be applied when collected by a
+     *                 {@link Character}
      */
     public DropImpl(final Level lv, final Vector2 position, final Buff buff) {
         super(lv, position, DIMENSIONS.width(), DIMENSIONS.height(), VELOCITY.x(), VELOCITY.y(),
@@ -50,17 +56,20 @@ public class DropImpl extends MovableImpl implements Drop {
         this.buff = buff;
 
         this.name = "drop-timer-" + UUID.randomUUID();
-        super.getLevel().getTimerManager().addTimer(this.name, new CustomTimerImpl(5000, () -> super.getLevel().removeGameObject(this)));
+        super.getLevel().getTimerManager().addTimer(this.name,
+                new CustomTimerImpl(5000, () -> super.getLevel().removeGameObject(this)));
     }
 
     /**
      * {@inheritDoc}
      *
      * <ul>
-     *     <li>If the object is a {@link Character}, the {@link Buff} is applied,
-     *     the drop is removed from the level, and its associated timer is cancelled.</li>
-     *     <li>If the object is a {@link Block} and the collision is from below (i.e. the drop lands on it),
-     *     the vertical movement is stopped.</li>
+     * <li>If the object is a {@link Character}, the {@link Buff} is applied,
+     * the drop is removed from the level, and its associated timer is
+     * cancelled.</li>
+     * <li>If the object is a {@link Block} and the collision is from below (i.e.
+     * the drop lands on it),
+     * the vertical movement is stopped.</li>
      * </ul>
      */
     @Override
