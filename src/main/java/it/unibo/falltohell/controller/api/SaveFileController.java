7239763db89e0df0a626073f3e5c07ac626dbf59
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-19 16:00:38 +0200

    The interface doesn't extend the FileController interface anymore

--- src/main/java/it/unibo/falltohell/controller/api/SaveFileController.java
+++ src/main/java/it/unibo/falltohell/controller/api/SaveFileController.java
@@ -4,7 +4,7 @@ package it.unibo.falltohell.controller.api;
  * Controller that saves the current state of the game in the save file.
  * @author Martina Malagoli
  */
-public interface SaveFileController extends FileController {
+public interface SaveFileController {
 
     /**
      * Method to save the current state of the game in the save file.
