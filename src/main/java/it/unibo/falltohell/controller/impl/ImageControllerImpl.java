c8a679b3dd0c5c9fe1c9e66e7a0f32cbb472673f
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-19 15:58:49 +0200

    Changed the createImageMap method in loadImage as the logic of the object changed as well

--- src/main/java/it/unibo/falltohell/controller/impl/ImageControllerImpl.java
+++ src/main/java/it/unibo/falltohell/controller/impl/ImageControllerImpl.java
@@ -2,16 +2,25 @@ package it.unibo.falltohell.controller.impl;
 
 import it.unibo.falltohell.controller.api.ImageController;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
+/**
+ * Controller that handles the loading of an image from the file system.
+ * @author Martina Malagoli
+ */
 public class ImageControllerImpl implements ImageController {
 
-    private static final String IMAGE_DIR_PATH = "src/main/resources/sprites/";
+    private static final String IMAGE_DIR_PATH = "src/main/resources/images/";
 
+    /**
+     *{@inheritDoc}
+     */
     @Override
-    public Map<String, Image> createImageMap() {
-        //TODO --> change method
-        return Map.of();
+    public Image loadImage(String fileName) throws IOException {
+        return ImageIO.read(new File(IMAGE_DIR_PATH + fileName));
     }
 }
