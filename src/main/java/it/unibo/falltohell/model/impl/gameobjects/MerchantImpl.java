eeabb249ef6fa3abc526cc3f935359ee0ea46490
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-21 18:28:28 +0200

    Finished the implementation of the class

--- src/main/java/it/unibo/falltohell/model/impl/gameobjects/MerchantImpl.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/MerchantImpl.java
@@ -5,13 +5,20 @@ import it.unibo.falltohell.controller.impl.FileControllerImpl;
 import it.unibo.falltohell.model.api.Level;
 import it.unibo.falltohell.model.api.gameobjects.Item;
 import it.unibo.falltohell.model.api.gameobjects.Merchant;
+import it.unibo.falltohell.model.api.gameobjects.movable.entity.statistic.CharacterStatistics;
+import it.unibo.falltohell.model.api.gameobjects.movable.entity.statistic.buff.Buff;
 import it.unibo.falltohell.model.api.physics.Collider;
 import it.unibo.falltohell.model.impl.GameObjectImpl;
+import it.unibo.falltohell.model.impl.gameobjects.interactable.Potion;
+import it.unibo.falltohell.model.impl.gameobjects.movable.entity.statistics.buff.*;
+import it.unibo.falltohell.model.impl.physics.colliders.BoxCollider;
+import it.unibo.falltohell.util.Dimensions;
 import it.unibo.falltohell.util.Vector2;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.io.IOException;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Class that represents the merchant that handles the character's purchase
@@ -20,10 +27,14 @@ import java.util.List;
  */
 public class MerchantImpl extends GameObjectImpl implements Merchant {
 
-    private static final String PATH = "src/main/resources/merchant";
-    private final FileController fileController;
+    private static final String PATH = "src/main/resources/merchant/potions.txt";
+    private static final Dimensions POTION_DIMENSION = new Dimensions(10, 10);
+    private static final double DISTANCE_FROM_ITEMS = 3;
+    private static final int NUMBER_ITEMS_AVAILABLE = 3;
+    private static final double BUFF_VALUE = 0.3;
     private final List<Item> merch;
-
+    private final List<String> allMerchFromFile;
+    private int potionCounter;
     /**
      * Initialization of the Merchant class.
      * @param lv is the current level
@@ -35,8 +46,15 @@ public class MerchantImpl extends GameObjectImpl implements Merchant {
     public MerchantImpl(final Level lv, final Vector2 position, final double width,
                         final double height, final Collider collider) {
         super(lv, position, width, height, collider);
-        this.fileController = new FileControllerImpl(PATH);
+        final FileController fileController = new FileControllerImpl();
+        try {
+            this.allMerchFromFile = fileController.read(PATH);
+        } catch (IOException e) {
+            throw new IllegalStateException("There is no potion file with in this path:" + PATH);
+        }
         this.merch = new ArrayList<>();
+        this.potionCounter = 0;
+        this.restock();
     }
 
     /**
@@ -44,7 +62,15 @@ public class MerchantImpl extends GameObjectImpl implements Merchant {
      */
     @Override
     public void restock() {
-        //TODO --> serve la classe Entrance --> poi ci va la lettura da file
+        this.potionCounter = 0;
+        this.merch.clear();
+        final List<String> shuffledMerch = new ArrayList<>(this.allMerchFromFile);
+        Collections.shuffle(shuffledMerch);
+        this.merch.addAll(shuffledMerch
+                .stream()
+                .limit(NUMBER_ITEMS_AVAILABLE)
+                .map(this::parseItem)
+                .toList());
     }
 
     /**
@@ -60,12 +86,52 @@ public class MerchantImpl extends GameObjectImpl implements Merchant {
      * Method to remove items if they are marked as sold.
      */
     private void sell() {
-        Iterator<Item> merchIterator = this.merch.iterator();
-        while (merchIterator.hasNext()) {
-            final Item item = merchIterator.next();
-            if (item.isSold()) {
-                merchIterator.remove();
-            }
+        this.merch.removeIf(Item::isSold);
+    }
+
+    /**
+     * Method to compute the position of an item.
+     * @return the item's position
+     */
+    private Vector2 computePosition() {
+        potionCounter++;
+        final double itemY = this.getPosition().y();
+        final double itemX = this.getPosition().x() - TILE_SIZE * potionCounter * DISTANCE_FROM_ITEMS;
+        return new Vector2(itemX, itemY);
+    }
+
+    /**
+     * Method to create a new item depending on the type.
+     * @param itemFileRow is the row with an item's information in the file
+     * @return the new item
+     */
+    private Item parseItem(final String itemFileRow) {
+        final String[] elements = itemFileRow.split(",");
+        final String type = elements[0];
+        final String cost = elements[1];
+        final Collider potionCollider = new BoxCollider(Vector2.zero(), POTION_DIMENSION);
+        final Buff buff;
+        final CharacterStatistics currentCharacterStats = (CharacterStatistics) this.getLevel()
+                .getGameData()
+                .getCurrentCharacter()
+                .getStats();
+        if (type.equalsIgnoreCase("life")) {
+            buff = new LifeBuff(currentCharacterStats, BUFF_VALUE);
+        } else if (type.equalsIgnoreCase("attack")) {
+            buff = new AttackBuff(currentCharacterStats, BUFF_VALUE);
+        } else if (type.equalsIgnoreCase("attsp")) {
+            buff = new AttackSpeedBuff(currentCharacterStats, BUFF_VALUE);
+        } else if (type.equalsIgnoreCase("speed")) {
+            buff = new SpeedBuff(currentCharacterStats, BUFF_VALUE);
+        } else if (type.equalsIgnoreCase("mana")) {
+            buff = new ManaBuff(currentCharacterStats, BUFF_VALUE);
+        } else {
+            throw new IllegalArgumentException("The row passed is not correct: there is no item with this name");
         }
+        return new Potion(this.getLevel(),
+                this.computePosition(),
+                0, 0,
+                potionCollider,
+                Long.parseLong(cost), buff);
     }
 }
