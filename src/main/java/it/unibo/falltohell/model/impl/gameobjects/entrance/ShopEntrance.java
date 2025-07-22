705d15688eb0975fae4eac22a7c36e585cb35d7d
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-22 18:24:04 +0200

    Added the ShopEntrance class

---
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/entrance/ShopEntrance.java
@@ -0,0 +1,56 @@
+package it.unibo.falltohell.model.impl.gameobjects.entrance;
+
+import it.unibo.falltohell.model.api.GameObject;
+import it.unibo.falltohell.model.api.Level;
+import it.unibo.falltohell.model.api.gameobjects.Merchant;
+import it.unibo.falltohell.model.api.physics.Collider;
+import it.unibo.falltohell.model.impl.GameObjectImpl;
+import it.unibo.falltohell.util.Vector2;
+
+import java.util.Optional;
+
+/**
+ * Class that represents the entrance of the shop.
+ * @author Martina Malagoli
+ */
+public class ShopEntrance extends GameObjectImpl {
+
+    private Optional<Merchant> merchant;
+
+    /**
+     * Initialization of the ShopEntrance class.
+     * @param lv is the level of the entrance
+     * @param position is the position of the entrance in the level
+     * @param width
+     * @param height
+     * @param collider associated with this entrance
+     */
+    public ShopEntrance(final Level lv, final Vector2 position,
+                        final double width, final double height, final Collider collider) {
+        super(lv, position, width, height, false, collider);
+        merchant = Optional.empty();
+    }
+
+    /**
+     *{@inheritDoc}
+     * It is used to restock the merch of the merchant when the
+     * character enters the shop and to destock it when the character
+     * walks away from it.
+     */
+    @Override
+    public void onCollisionExit(GameObject other, Vector2 direction) {
+        if (direction.equals(Vector2.right())) {
+            this.merchant.ifPresent(Merchant::restock);
+        } else {
+            this.merchant.ifPresent(Merchant::destock);
+        }
+    }
+
+    /**
+     * Method add the merchant to this class.
+     * @param merchant of the same level as this entrance
+     */
+    public void setMerchant(final Merchant merchant) {
+        this.merchant = Optional.of(merchant);
+    }
+}
