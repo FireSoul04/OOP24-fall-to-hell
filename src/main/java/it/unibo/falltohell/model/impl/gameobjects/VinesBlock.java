37182d2c2d9d812b4424bc7b157869a887fd6bdc
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-22 17:35:22 +0200

    Added the VinesBlock class

---
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/VinesBlock.java
@@ -0,0 +1,56 @@
+package it.unibo.falltohell.model.impl.gameobjects;
+
+import it.unibo.falltohell.model.api.GameObject;
+import it.unibo.falltohell.model.api.Level;
+import it.unibo.falltohell.model.api.gameobjects.movable.Entity;
+import it.unibo.falltohell.model.api.gameobjects.movable.entity.Statistics;
+import it.unibo.falltohell.model.api.physics.Collider;
+import it.unibo.falltohell.util.Vector2;
+
+/**
+ * Class that represents a block that reduces the speed of an entity
+ * when it walks over it.
+ * @author Martina Malagoli.
+ */
+public class VinesBlock extends BaseBlock {
+
+    private static final double MULTIPLIER = 0.3;
+
+    /**
+     * Initialization of the VinesBlock class.
+     * @param lv is the level of the block
+     * @param position is the position of the block in the level
+     * @param width
+     * @param height
+     * @param collider associated to the block
+     */
+    public VinesBlock(Level lv, Vector2 position, double width, double height, Collider collider) {
+        super(lv, position, width, height, collider);
+    }
+
+    /**
+     * {@inheritDoc}
+     * It is used to reduce the speed of an entity when it collides
+     * with this type of block.
+     */
+    @Override
+    public void onCollision(GameObject other, Vector2 direction) {
+        if (direction.equals(Vector2.up()) && other instanceof Entity entity) {
+            final Statistics statistics = entity.getStats();
+            statistics.setSpeed(statistics.getInitialSpeed().multiply(MULTIPLIER));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * It is used to reset the normal speed of an entity when it
+     * walks away from this block.
+     */
+    @Override
+    public void onCollisionExit(GameObject other, Vector2 direction) {
+        if (other instanceof Entity entity) {
+            final Statistics statistics = entity.getStats();
+            statistics.setSpeed(statistics.getInitialSpeed());
+        }
+    }
+}
