3d84300843451bf779302d145f23c11886d2c3a0
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-24 16:50:35 +0200

    Added the call to the listener in the collision methods and changed their extended class from GameObject to BaseEntrance

--- src/main/java/it/unibo/falltohell/model/impl/gameobjects/entrance/ShopEntrance.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/entrance/ShopEntrance.java
@@ -1,11 +1,9 @@
 package it.unibo.falltohell.model.impl.gameobjects.entrance;
 
-import it.unibo.falltohell.model.api.Drawable;
 import it.unibo.falltohell.model.api.GameObject;
 import it.unibo.falltohell.model.api.Level;
 import it.unibo.falltohell.model.api.gameobjects.Merchant;
 import it.unibo.falltohell.model.api.physics.Collider;
-import it.unibo.falltohell.model.impl.GameObjectImpl;
 import it.unibo.falltohell.util.Vector2;
 
 import java.util.Optional;
@@ -14,7 +12,7 @@ import java.util.Optional;
  * Class that represents the entrance of the shop.
  * @author Martina Malagoli
  */
-public class ShopEntrance extends GameObjectImpl {
+public class ShopEntrance extends BaseEntrance {
 
     private Optional<Merchant> merchant;
 
@@ -22,13 +20,10 @@ public class ShopEntrance extends GameObjectImpl {
      * Initialization of the ShopEntrance class.
      * @param lv is the level of the entrance
      * @param position is the position of the entrance in the level
-     * @param width
-     * @param height
      * @param collider associated with this entrance
      */
-    public ShopEntrance(final Level lv, final Vector2 position,
-                        final Collider collider) {
-        super(lv, position, false, collider);
+    public ShopEntrance(final Level lv, final Vector2 position, final Collider collider) {
+        super(lv, position, collider);
         merchant = Optional.empty();
     }
 
@@ -42,8 +37,10 @@ public class ShopEntrance extends GameObjectImpl {
     public void onCollisionExit(GameObject other, Vector2 direction) {
         if (direction.equals(Vector2.right())) {
             this.merchant.ifPresent(Merchant::restock);
+            this.getListener().call();
         } else {
             this.merchant.ifPresent(Merchant::destock);
+            this.getListener().call();
         }
     }
 
