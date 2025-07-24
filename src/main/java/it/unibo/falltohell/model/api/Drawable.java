aa05e08465bbf4a5eeae157552725cca9a7a3ee5
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-24 22:54:56 +0200

    Added the VERY_LOW element to the Priority enum and added some javadoc

--- src/main/java/it/unibo/falltohell/model/api/Drawable.java
+++ src/main/java/it/unibo/falltohell/model/api/Drawable.java
@@ -12,10 +12,30 @@ public interface Drawable {
      * Priority of the drawable object when it must be rendered.
      */
     enum Priority {
+        /**
+         * Is the priority given to GUI elements.
+         */
         GUI,
+        /**
+         * Is the priority given to blocks.
+         */
         HIGH,
+        /**
+         * Is the priority given to enemies, to weapons and to projectiles.
+         */
         MEDIUM,
+        /**
+         * Is the priority given to the character.
+         */
         LOW,
+        /**
+         * Is the priority given to the save point, to the character changer,
+         * to items and to drops.
+         */
+        VERY_LOW,
+        /**
+         * Is the priority given to the background.
+         */
         BACKGROUND
     }
 
