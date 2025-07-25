05296ec271d8b615e277d2cbbdde73f9ebe4a5fe
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-25 17:01:19 +0200

    Added the getPriority method

--- src/main/java/it/unibo/falltohell/view/impl/LabelView.java
+++ src/main/java/it/unibo/falltohell/view/impl/LabelView.java
@@ -1,5 +1,6 @@
 package it.unibo.falltohell.view.impl;
 
+import it.unibo.falltohell.util.Priority;
 import it.unibo.falltohell.util.Vector2;
 import java.awt.Graphics;
 import java.awt.Color;
@@ -12,7 +13,7 @@ import it.unibo.falltohell.model.impl.drawable.Label;
  * @author Casadei Lorenzo
  */
 public class LabelView extends BaseRenderable {
-    private Label text;
+    private final Label text;
     
     /**
      * Constructor for the LabelView.
@@ -61,6 +62,12 @@ public class LabelView extends BaseRenderable {
     public void translate(final Vector2 newPosition) {
         // Labels typically do not move, but if needed, this can be implemented.
     }
+
+    @Override
+    public Priority getPriority() {
+        return Priority.GUI;
+    }
+
     /**
      * Method to render the label on the screen.
      * @param g the graphics context to render the label.
