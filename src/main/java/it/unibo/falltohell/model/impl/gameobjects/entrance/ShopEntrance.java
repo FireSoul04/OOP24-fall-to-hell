b967adcadd407bda8a3ce287ba42ec40e292c3c3
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-25 22:18:31 +0200

    Fixed bugs

--- src/main/java/it/unibo/falltohell/model/impl/gameobjects/entrance/ShopEntrance.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/entrance/ShopEntrance.java
@@ -3,6 +3,7 @@ package it.unibo.falltohell.model.impl.gameobjects.entrance;
 import it.unibo.falltohell.model.api.GameObject;
 import it.unibo.falltohell.model.api.Level;
 import it.unibo.falltohell.model.api.gameobjects.Merchant;
+import it.unibo.falltohell.model.api.gameobjects.movable.entity.Character;
 import it.unibo.falltohell.model.api.physics.Collider;
 import it.unibo.falltohell.util.Vector2;
 
@@ -35,12 +36,14 @@ public class ShopEntrance extends BaseEntrance {
      */
     @Override
     public void onCollisionExit(final GameObject other, final Vector2 direction) {
-        if (direction.equals(Vector2.right())) {
-            this.merchant.ifPresent(Merchant::restock);
-            this.getListener().call();
-        } else if (direction.equals(Vector2.left())) {
-            this.merchant.ifPresent(Merchant::destock);
-            this.getListener().call();
+        if (other instanceof Character) {
+            if (direction.equals(Vector2.left())) {
+                this.merchant.ifPresent(Merchant::restock);
+                this.getListener().call();
+            } else if (direction.equals(Vector2.right())) {
+                this.merchant.ifPresent(Merchant::destock);
+                this.getListener().call();
+            }
         }
     }
 
--- src/main/java/it/unibo/falltohell/model/impl/gameobjects/entrance/SpringsEntrance.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/entrance/SpringsEntrance.java
@@ -2,6 +2,7 @@ package it.unibo.falltohell.model.impl.gameobjects.entrance;
 
 import it.unibo.falltohell.model.api.GameObject;
 import it.unibo.falltohell.model.api.Level;
+import it.unibo.falltohell.model.api.gameobjects.movable.entity.Character;
 import it.unibo.falltohell.model.api.gameobjects.movable.entity.Statistics;
 import it.unibo.falltohell.model.api.physics.Collider;
 import it.unibo.falltohell.util.Vector2;
@@ -28,12 +29,14 @@ public class SpringsEntrance extends BaseEntrance {
      */
     @Override
     public void onCollisionExit(final GameObject other, final Vector2 direction) {
-        final Statistics statistics = this.getLevel().getGameData().getCurrentCharacter().getStats();
-        if (direction.equals(Vector2.right())) {
-            statistics.setLife(statistics.getFullLife());
-            this.getListener().call();
-        } else if (direction.equals(Vector2.left())) {
-            this.getListener().call();
+        if (other instanceof Character) {
+            final Statistics statistics = this.getLevel().getGameData().getCurrentCharacter().getStats();
+            if (direction.equals(Vector2.right())) {
+                statistics.setLife(statistics.getFullLife());
+                this.getListener().call();
+            } else if (direction.equals(Vector2.left())) {
+                this.getListener().call();
+            }
         }
     }
 }
