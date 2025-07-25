9998ee9734921959ef070684a6da7542a3921929
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-26 00:55:30 +0200

    Renamed the BlockFactoryImpl class in CollidableBlockFactoryImpl

--- src/main/java/it/unibo/falltohell/model/impl/gameobjects/block/CollidableBlockFactoryImpl.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/block/CollidableBlockFactoryImpl.java
@@ -9,7 +9,7 @@ import it.unibo.falltohell.util.Vector2;
  * Class that handles the creation of different types of block.
  * @author Martina Malagoli
  */
-public class BlockFactoryImpl implements BlockFactory {
+public class CollidableBlockFactoryImpl implements BlockFactory {
 
     private static final Vector2 OFFSET_LAVA = new Vector2(0.0, 0.5);
     private static final Vector2 OFFSET_VINES = new Vector2(0.0, 3.0);
@@ -18,23 +18,23 @@ public class BlockFactoryImpl implements BlockFactory {
      *{@inheritDoc}
      */
     @Override
-    public BaseBlock createBaseBlock(final Level level, final Vector2 position) {
-        return new BaseBlock(level, position, new BoxCollider(), "BaseBlock.png");
+    public BaseCollidableBlock createBaseBlock(final Level level, final Vector2 position) {
+        return new BaseCollidableBlock(level, position, new BoxCollider(), "base_block.png");
     }
 
     /**
      *{@inheritDoc}
      */
     @Override
-    public BaseBlock createLavaBlock(final Level level, final Vector2 position) {
-        return new LavaBlock(level, position, new BoxCollider(), "LavaBlock.png", OFFSET_LAVA);
+    public BaseCollidableBlock createLavaBlock(final Level level, final Vector2 position) {
+        return new LavaBlock(level, position, new BoxCollider(), "lava_block.png", OFFSET_LAVA);
     }
 
     /**
      *{@inheritDoc}
      */
     @Override
-    public BaseBlock createVinesBlock(final Level level, final Vector2 position) {
-        return new VinesBlock(level, position, new BoxCollider(), "VinesBlock.png", OFFSET_VINES);
+    public BaseCollidableBlock createVinesBlock(final Level level, final Vector2 position) {
+        return new VinesBlock(level, position, new BoxCollider(), "vines_block.png", OFFSET_VINES);
     }
 }
