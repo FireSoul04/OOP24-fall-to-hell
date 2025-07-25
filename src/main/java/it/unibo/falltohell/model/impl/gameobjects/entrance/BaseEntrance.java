d44b9610da58a6465c0f322ba7af333a8e0642fa
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-25 23:39:55 +0200

    Added constants for collider dimension and offset and used a default collider created with those two constants in the constructor

--- src/main/java/it/unibo/falltohell/model/impl/gameobjects/entrance/BaseEntrance.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/entrance/BaseEntrance.java
@@ -5,6 +5,8 @@ import it.unibo.falltohell.model.api.gameobjects.movable.entity.AggroListener;
 import it.unibo.falltohell.model.api.physics.Collider;
 import it.unibo.falltohell.model.impl.EnemyFactoryImpl;
 import it.unibo.falltohell.model.impl.GameObjectImpl;
+import it.unibo.falltohell.model.impl.physics.colliders.BoxCollider;
+import it.unibo.falltohell.util.Dimensions;
 import it.unibo.falltohell.util.Vector2;
 
 /**
@@ -13,16 +15,17 @@ import it.unibo.falltohell.util.Vector2;
  */
 public class BaseEntrance extends GameObjectImpl {
 
+    private static final Dimensions DIMENSIONS = new Dimensions(20, 40);
+    private static final double OFFSET = 10;
     private final AggroListener listener;
 
     /**
      * Initialization of the BaseEntrance class.
      * @param lv is the level of the entrance
      * @param position is the position of the entrance in the level
-     * @param collider associated with this entrance
      */
-    public BaseEntrance(final Level lv, final Vector2 position, final Collider collider) {
-        super(lv, position, false, collider);
+    public BaseEntrance(final Level lv, final Vector2 position) {
+        super(lv, position, false, new BoxCollider(Vector2.up().multiply(OFFSET), DIMENSIONS));
         this.listener = new EnemyFactoryImpl().askManager(lv).addEntrance(this);
     }
 
