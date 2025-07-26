7993fa2cdb75f429e18a08fe45e9c86c7fa7d2ba
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-07-26 13:52:17 +0200

    Moved the throw knife ability logic to a new class ability

--- src/main/java/it/unibo/falltohell/model/impl/gameobjects/movable/entity/character/Rogue.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/movable/entity/character/Rogue.java
@@ -1,15 +1,18 @@
 package it.unibo.falltohell.model.impl.gameobjects.movable.entity.character;
 
 import it.unibo.falltohell.model.api.Level;
+import it.unibo.falltohell.model.api.abilities.active.SpecialActiveAbility;
+import it.unibo.falltohell.model.api.abilities.passive.StatisticPassiveAbility;
 import it.unibo.falltohell.model.api.gameobjects.movable.entity.statistic.CharacterStatistics;
+import it.unibo.falltohell.model.api.gameobjects.movable.entity.statistic.buff.Buff;
+import it.unibo.falltohell.model.impl.abilities.AbilityFactoryImpl;
+import it.unibo.falltohell.model.impl.abilities.active.ThrowKnifeAbility;
+import it.unibo.falltohell.model.impl.gameobjects.movable.entity.statistics.buff.SpeedBuff;
 import it.unibo.falltohell.model.impl.gameobjects.movable.entity.weapons.Dagger;
-import it.unibo.falltohell.model.impl.gameobjects.movable.projectile.Knife;
 import it.unibo.falltohell.model.impl.gameobjects.movable.entity.BaseCharacter;
 import it.unibo.falltohell.model.impl.gameobjects.movable.entity.StatisticFactoryImpl;
 import it.unibo.falltohell.util.Dimensions;
 import it.unibo.falltohell.util.Vector2;
-
-import java.util.List;
 
 /**
  * Character representing a rogue.
@@ -25,12 +28,9 @@ public class Rogue extends BaseCharacter {
     private static final Vector2 SPEED = new Vector2(2.0, 1.5);
     private static final CharacterStatistics STATS = new StatisticFactoryImpl()
         .createCharacterStatistic(LIFE, ATTACK, SPEED, new Dimensions(20, 25), MANA, ATTACK_SPEED);
-    private static final List<Vector2> KNIFES_VELOCITIES = List.of(
-        new Vector2(3.0, 0.0),
-        new Vector2(2.0, 1.0),
-        new Vector2(2.0, -1.0)
-    );
 
+    private final StatisticPassiveAbility evadeAbility;
+    private final SpecialActiveAbility knifeAbility;
     private boolean canDoubleJump;
 
     /**
@@ -43,6 +43,13 @@ public class Rogue extends BaseCharacter {
         super(level, position, STATS, "rogue.png");
         this.canDoubleJump = false;
         this.equipWeapon(new Dagger(level, position));
+
+        this.evadeAbility = new AbilityFactoryImpl()
+            .createPassiveAbility(this, character -> {
+                final Buff speedBuff = new SpeedBuff(STATS, 0.5);
+                this.getBuffManager().addBuff(speedBuff);
+            });
+        this.knifeAbility = new ThrowKnifeAbility(this);
     }
 
     /**
@@ -52,24 +59,18 @@ public class Rogue extends BaseCharacter {
     public void update(final double deltaTime) {
         super.update(deltaTime);
         this.doubleJump();
-        if (this.getInput().checkCondition("ActiveAbility")) {
-            this.throwKnifes();
+        if (this.getLevel().getGameEventManager().checkCondition("ActiveAbility")) {
+            this.knifeAbility.activate();
         }
     }
 
     private void doubleJump() {
-        if (this.getInput().checkCondition("Jump") && !this.isJumping() && this.canDoubleJump) {
+        if (this.getLevel().getGameEventManager().checkCondition("Jump") && !this.isJumping() && this.canDoubleJump) {
             this.resetJump();
             this.canDoubleJump = false;
         }
         if (this.isOnGround()) {
             this.canDoubleJump = true;
-        }
-    }
-
-    private void throwKnifes() {
-        for (final Vector2 v : KNIFES_VELOCITIES) {
-            new Knife(this.getLevel(), this.getPosition(), v);
         }
     }
 
@@ -79,5 +80,15 @@ public class Rogue extends BaseCharacter {
     @Override
     public CharacterID getCharacterID() {
         return CharacterID.ROGUE;
+    }
+
+    /**
+     * {@inheritDoc}
+     * Active passive ability on take damage.
+     */
+    @Override
+    public void setDamagedLife(double damage) {
+        super.setDamagedLife(damage);
+        this.evadeAbility.carryOut();
     }
 }
