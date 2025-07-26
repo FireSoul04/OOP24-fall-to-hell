dee76441b2f9631c6ab86a65a156f4ee0b918856
Sara Visani <sara.visani7@studio.unibo.it>
2025-07-26 17:40:45 +0200

    removed import

--- src/main/java/it/unibo/falltohell/controller/api/DrawableRenderableHandler.java
+++ src/main/java/it/unibo/falltohell/controller/api/DrawableRenderableHandler.java
@@ -4,7 +4,6 @@ import it.unibo.falltohell.model.api.Drawable;
 import it.unibo.falltohell.model.api.GameCamera;
 import it.unibo.falltohell.view.api.Renderable;
 
-import java.awt.Image;
 import java.util.List;
 
 /**
