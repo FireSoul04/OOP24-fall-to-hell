83eadfe296b2bf480131d0884315f079d3f0258f
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-07-26 14:18:21 +0200

    Added owner in constructor and fixed javadoc

--- src/main/java/it/unibo/falltohell/model/impl/gameobjects/movable/entity/weapons/Dagger.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/movable/entity/weapons/Dagger.java
@@ -1,6 +1,7 @@
 package it.unibo.falltohell.model.impl.gameobjects.movable.entity.weapons;
 
 import it.unibo.falltohell.model.api.Level;
+import it.unibo.falltohell.model.impl.gameobjects.movable.entity.character.Rogue;
 import it.unibo.falltohell.model.impl.physics.colliders.BoxCollider;
 import it.unibo.falltohell.util.Dimensions;
 import it.unibo.falltohell.util.Vector2;
@@ -10,13 +11,13 @@ public class Dagger extends BaseMeleeWeapon {
     private static final Dimensions SIZE = new Dimensions(15, 10);
 
     /**
-     * Creates an abstract close ranged weapon.
-     *
-     * @param lv       is the level where there is the melee weapon
-     * @param position is the position of the melee weapon in the level
+     * Creates an short range dagger.
+     * @param lv       is the level where there is the dagger
+     * @param position is the position of the dagger in the level
+     * @param owner    is the owner of the dagger
      */
-    public Dagger(final Level lv, final Vector2 position) {
-        super(lv, position, new BoxCollider(SIZE), "dagger.png");
+    public Dagger(final Level lv, final Vector2 position, final Rogue owner) {
+        super(lv, position, new BoxCollider(SIZE), owner, "dagger.png");
     }
 
     @Override
