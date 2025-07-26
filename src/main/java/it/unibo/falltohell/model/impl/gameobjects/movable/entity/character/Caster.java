aec8e8965d9d6e6dd37f99597e5fa686f8ce4b43
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-26 14:45:45 +0200

    Continued implementation

--- src/main/java/it/unibo/falltohell/model/impl/gameobjects/movable/entity/character/Caster.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/movable/entity/character/Caster.java
@@ -5,17 +5,20 @@ import it.unibo.falltohell.model.api.gameobjects.movable.entity.statistic.Charac
 import it.unibo.falltohell.model.impl.gameobjects.movable.entity.BaseCharacter;
 import it.unibo.falltohell.model.impl.gameobjects.movable.entity.StatisticFactoryImpl;
 import it.unibo.falltohell.model.impl.gameobjects.movable.entity.weapons.Staff;
+import it.unibo.falltohell.model.impl.gameobjects.movable.projectile.Fireball;
 import it.unibo.falltohell.util.Dimensions;
 import it.unibo.falltohell.util.Vector2;
 
 public class Caster extends BaseCharacter {
 
-    private static final double LIFE = 10;
+    private static final double LIFE = 50;
     private static final double ATTACK = 20;
     private static final double ATTACK_SPEED = 8;
     private static final Vector2 SPEED = new Vector2(2, 2);
-    private static final double MANA = 20;
-    private static final double AMOUNT_MANA_NORMAL_ATTACK = 2;
+    private static final double MANA = 25;
+    private static final double AMOUNT_MANA_NORMAL_ATTACK = 1;
+    private static final double AMOUNT_MANA_SPECIAL_ATTACK = 20;
+    private static final double AMOUNT_MANA_HEAL = 10;
     private static final CharacterStatistics STATISTICS = new StatisticFactoryImpl() //TODO --> si modificano se è costante? Mi sa va cambiato
             .createCharacterStatistic(LIFE, ATTACK, SPEED, new Dimensions(20,20), MANA, ATTACK_SPEED);
     private final Staff staff;
@@ -38,7 +41,9 @@ public class Caster extends BaseCharacter {
 
     public void attack() {
         if (STATISTICS.getMana() + STATISTICS.getTemporaryMana() >= AMOUNT_MANA_NORMAL_ATTACK) {
-
+            
+        } else {
+            this.staff.attack();
         }
     }
 }
