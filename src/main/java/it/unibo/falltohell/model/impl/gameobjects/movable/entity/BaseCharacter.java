19bfef491bf8f95d5fcf49cef041d7ce14e9e412
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-26 17:18:06 +0200

    Added the setting of the isFacingRight in the move method

--- src/main/java/it/unibo/falltohell/model/impl/gameobjects/movable/entity/BaseCharacter.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/movable/entity/BaseCharacter.java
@@ -90,9 +90,11 @@ public abstract class BaseCharacter extends EntityImpl implements Character {
         Vector2 moveVelocity = Vector2.zero();
         if (this.input.checkCondition("MoveLeft")) {
             moveVelocity = moveVelocity.add(Vector2.left());
+            this.setFacingRight(false);
         }
         if (this.input.checkCondition("MoveRight")) {
             moveVelocity = moveVelocity.add(Vector2.right());
+            this.setFacingRight(true);
         }
         moveVelocity = moveVelocity.multiply(this.getStats().getSpeed().x()).multiply(deltaTime);
         this.velocity = this.velocity.add(moveVelocity);
