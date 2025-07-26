15998558b13dd3d7ef3a6765bd3a165e7f488d94
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-26 17:12:57 +0200

    Added the HealAbility class

---
+++ src/main/java/it/unibo/falltohell/model/impl/abilities/active/HealAbility.java
@@ -0,0 +1,18 @@
+package it.unibo.falltohell.model.impl.abilities.active;
+
+import it.unibo.falltohell.model.api.abilities.active.SpecialActiveAbility;
+import it.unibo.falltohell.model.impl.gameobjects.movable.entity.character.Caster;
+
+public class HealAbility implements SpecialActiveAbility {
+
+    private static final Caster caster;
+
+    public HealAbility(Caster caster) {
+
+    }
+
+    @Override
+    public void activate() {
+
+    }
+}
