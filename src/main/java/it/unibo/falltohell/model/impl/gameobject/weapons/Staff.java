bdd342d4c3933b0625515fdf2a600d7c5b94b173
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-26 23:39:54 +0200

    Removed unused class parameter and added some javadoc

--- src/main/java/it/unibo/falltohell/model/impl/gameobject/weapons/Staff.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobject/weapons/Staff.java
@@ -4,13 +4,15 @@ import it.unibo.falltohell.model.impl.gameobject.movable.entity.character.Caster
 import it.unibo.falltohell.model.impl.physics.BoxCollider;
 import it.unibo.falltohell.util.Dimensions;
 
-
+/**
+ * Class that represents a caster's staff.
+ * @author Martina Malagoli
+ */
 public class Staff extends BaseMeleeWeapon {
 
     private static final double DAMAGE_MULTIPLIER = 0.3;
     private static final Dimensions DIMENSIONS = new Dimensions(3, 10);
     private static final long COOLDOWN = 600;
-    private final Caster caster;
 
     /**
      * Creates a staff.
@@ -19,7 +21,5 @@ public class Staff extends BaseMeleeWeapon {
      */
     public Staff(final Caster caster) {
         super(caster, new BoxCollider(DIMENSIONS), DAMAGE_MULTIPLIER, COOLDOWN ,"staff.png");
-        this.caster = caster;
     }
-
 }
