178cf3c9668da69e4dea3c061fbdb0e579a255a8
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-07-26 12:14:43 +0200

    Added dagger weapon

---
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/movable/entity/weapons/Dagger.java
@@ -0,0 +1,26 @@
+package it.unibo.falltohell.model.impl.gameobjects.movable.entity.weapons;
+
+import it.unibo.falltohell.model.api.Level;
+import it.unibo.falltohell.model.impl.physics.colliders.BoxCollider;
+import it.unibo.falltohell.util.Dimensions;
+import it.unibo.falltohell.util.Vector2;
+
+public class Dagger extends MeleeWeapon {
+
+    private static final Dimensions SIZE = new Dimensions(15, 10);
+
+    /**
+     * Creates an abstract close ranged weapon.
+     *
+     * @param lv       is the level where there is the melee weapon
+     * @param position is the position of the melee weapon in the level
+     */
+    public Dagger(final Level lv, final Vector2 position) {
+        super(lv, position, new BoxCollider(SIZE), "dagger.png");
+    }
+
+    @Override
+    public void attack() {
+        super.attack();
+    }
+}
