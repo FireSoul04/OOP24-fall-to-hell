d33e15e58eefa2b67fe636118f4c7f66a1d68896
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-07-26 15:37:17 +0200

    Fixed dagger to the new MeleeWeapon constructor

--- src/main/java/it/unibo/falltohell/model/impl/gameobjects/movable/entity/weapons/Dagger.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobjects/movable/entity/weapons/Dagger.java
@@ -1,30 +1,20 @@
 package it.unibo.falltohell.model.impl.gameobjects.movable.entity.weapons;
 
-import it.unibo.falltohell.model.api.Level;
 import it.unibo.falltohell.model.impl.gameobjects.movable.entity.character.Rogue;
 import it.unibo.falltohell.model.impl.physics.colliders.BoxCollider;
 import it.unibo.falltohell.util.Dimensions;
-import it.unibo.falltohell.util.Vector2;
 
 public class Dagger extends BaseMeleeWeapon {
 
+    private static final long COOLDOWN_TIME = 400;
+    private static final double DAMAGE_MULTIPLIER = 0.8;
     private static final Dimensions SIZE = new Dimensions(15, 10);
 
     /**
      * Creates an short range dagger.
-     * @param lv       is the level where there is the dagger
-     * @param position is the position of the dagger in the level
      * @param owner    is the owner of the dagger
      */
-    public Dagger(final Level lv, final Vector2 position, final Rogue owner) {
-        super(lv, position, new BoxCollider(SIZE), owner, "dagger.png");
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void attack() {
-        super.attack();
+    public Dagger(final Rogue owner) {
+        super(owner, new BoxCollider(SIZE), DAMAGE_MULTIPLIER, COOLDOWN_TIME, "dagger.png");
     }
 }
