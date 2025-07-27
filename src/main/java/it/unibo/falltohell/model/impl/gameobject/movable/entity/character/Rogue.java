f9e27e42021ff212c8b684b6ae5c8d127bef4aa3
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-07-27 14:54:12 +0200

    Moved the passive ability activation to onCollision with enemy or enemy projectile and added a cooldown to the passive ability activation

--- src/main/java/it/unibo/falltohell/model/impl/gameobject/movable/entity/character/Rogue.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobject/movable/entity/character/Rogue.java
@@ -1,15 +1,20 @@
 package it.unibo.falltohell.model.impl.gameobject.movable.entity.character;
 
+import it.unibo.falltohell.model.api.gameobject.GameObject;
+import it.unibo.falltohell.model.api.gameobject.movable.entity.enemy.Enemy;
 import it.unibo.falltohell.model.api.level.Level;
 import it.unibo.falltohell.model.api.ability.active.SpecialActiveAbility;
 import it.unibo.falltohell.model.api.ability.passive.StatisticPassiveAbility;
+import it.unibo.falltohell.model.api.manager.TimerManager;
 import it.unibo.falltohell.model.api.statistic.CharacterStatistics;
 import it.unibo.falltohell.model.api.buff.Buff;
 import it.unibo.falltohell.model.impl.factory.AbilityFactoryImpl;
 import it.unibo.falltohell.model.impl.ability.active.ThrowKnifeAbility;
 import it.unibo.falltohell.model.impl.buff.SpeedBuff;
+import it.unibo.falltohell.model.impl.gameobject.movable.projectile.BaseEnemyProjectile;
 import it.unibo.falltohell.model.impl.gameobject.weapons.Dagger;
 import it.unibo.falltohell.model.impl.factory.StatisticFactoryImpl;
+import it.unibo.falltohell.model.impl.timer.CustomTimerImpl;
 import it.unibo.falltohell.util.Dimensions;
 import it.unibo.falltohell.util.Vector2;
 
@@ -28,9 +33,12 @@ public class Rogue extends BaseCharacter {
     private static final CharacterStatistics STATS = new StatisticFactoryImpl()
         .createCharacterStatistic(LIFE, ATTACK, SPEED, new Dimensions(20, 25), MANA, ATTACK_SPEED);
 
+    private static final long PASSIVE_COOLDOWN_TIME = 2000;
+
     private final StatisticPassiveAbility evadeAbility;
     private final SpecialActiveAbility knifeAbility;
     private boolean canDoubleJump;
+    private boolean canUsePassive;
 
     /**
      * Creates a rogue.
@@ -41,11 +49,25 @@ public class Rogue extends BaseCharacter {
     public Rogue(final Level level, final Vector2 position) {
         super(level, position, STATS, "rogue.png");
         this.canDoubleJump = false;
+        this.canUsePassive = true;
         this.equipWeapon(new Dagger(this));
         this.evadeAbility = new AbilityFactoryImpl()
             .createPassiveAbility(this, character -> {
-                final Buff speedBuff = new SpeedBuff(STATS, 0.5);
-                this.getBuffManager().addBuff(speedBuff);
+                if (this.canUsePassive) {
+                    final TimerManager tm = this.getLevel().getTimerManager();
+                    final String passiveCooldownTimerName = "cooldown";
+                    final Buff speedBuff = new SpeedBuff(STATS, 0.5);
+                    this.getBuffManager().addBuff(speedBuff);
+                    this.canUsePassive = false;
+                    if (!tm.searchTimer(passiveCooldownTimerName)) {
+                        tm.addTimer(
+                            passiveCooldownTimerName,
+                            new CustomTimerImpl(PASSIVE_COOLDOWN_TIME, () -> this.canUsePassive = true)
+                        );
+                    } else {
+                        tm.restartTimer(passiveCooldownTimerName);
+                    }
+                }
             });
         this.knifeAbility = new ThrowKnifeAbility(this);
     }
@@ -87,6 +109,13 @@ public class Rogue extends BaseCharacter {
     @Override
     public void setDamagedLife(double damage) {
         super.setDamagedLife(damage);
-        this.evadeAbility.carryOut();
+    }
+
+    @Override
+    public void onCollision(GameObject other, Vector2 direction) {
+        super.onCollision(other, direction);
+        if (other instanceof Enemy || other instanceof BaseEnemyProjectile) {
+            this.evadeAbility.carryOut();
+        }
     }
 }
