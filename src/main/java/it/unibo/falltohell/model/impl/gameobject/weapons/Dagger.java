2875b25e7b25536a4654a83d5e4bbcd4eb08c1f8
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-07-28 23:54:41 +0200

    Balanced the dagger attack timer

--- src/main/java/it/unibo/falltohell/model/impl/gameobject/weapons/Dagger.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobject/weapons/Dagger.java
@@ -7,7 +7,7 @@ import it.unibo.falltohell.util.Vector2;
 
 public class Dagger extends BaseMeleeWeapon {
 
-    private static final long COOLDOWN_TIME = 400;
+    private static final long COOLDOWN_TIME = 800;
     private static final double DAMAGE_MULTIPLIER = 0.8;
     private static final Dimensions SIZE = new Dimensions(20, 12);
     private static final Vector2 OFFSET = new Vector2(10, 0);
