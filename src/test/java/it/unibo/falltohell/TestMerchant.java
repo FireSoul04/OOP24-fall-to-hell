28046e3cb2ca19ef246261d340af63eec250e8c1
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-29 00:01:30 +0200

    Added some javadoc and messages

--- src/test/java/it/unibo/falltohell/TestMerchant.java
+++ src/test/java/it/unibo/falltohell/TestMerchant.java
@@ -1,12 +1,14 @@
 package it.unibo.falltohell;
 
+import it.unibo.falltohell.model.api.GameData;
+import it.unibo.falltohell.model.api.gameobject.GameObject;
 import it.unibo.falltohell.model.api.level.Level;
 import it.unibo.falltohell.model.api.gameobject.movable.entity.character.Character;
 import it.unibo.falltohell.model.api.gameobject.movable.entity.character.Character.CharacterID;
 import it.unibo.falltohell.model.api.statistic.CharacterStatistics;
 import it.unibo.falltohell.model.impl.GameDataImpl;
-import it.unibo.falltohell.model.impl.gameobject.entrance.BaseEntrance;
 import it.unibo.falltohell.model.impl.gameobject.entrance.ShopEntrance;
+import it.unibo.falltohell.model.impl.gameobject.movable.entity.character.Caster;
 import it.unibo.falltohell.model.impl.gameobject.movable.entity.character.Druid;
 import it.unibo.falltohell.model.impl.gameobject.movable.entity.character.Rogue;
 import it.unibo.falltohell.model.impl.physics.BoxCollider;
@@ -29,6 +31,9 @@ class TestMerchant {
     private MerchantTest merchant;
     private Character character;
 
+    /**
+     * Initialization of the variables used in the tests.
+     */
     @BeforeEach
     void initialization() {
         final Level level = new LevelTest();
@@ -40,27 +45,69 @@ class TestMerchant {
         this.character = new Rogue(level, Vector2.zero());
     }
 
+    /**
+     * Tests if the purchase happens if the character has enough points and if potions apply buffs
+     * correctly when purchased.
+     */
     @Test
-    void TestSellAndAcquisitionOfBuff() {
+    void testSellWithPointsAndAcquisitionOfBuff() {
+        this.merchant.restock();
         final int initialNumberOfItems = this.merchant.getMerch().size();
         final long initialPoints = this.merchant.getLevel().getGameData().getPoints();
         this.merchant.getMerch().get(0).interact(this.character);
         this.merchant.update();
-        Assertions.assertEquals(initialNumberOfItems - 1, this.merchant.getMerch().size());
+        Assertions.assertEquals(initialNumberOfItems - 1, this.merchant.getMerch().size(),
+                "The number of items should be the on");
         final CharacterStatistics statistics = (CharacterStatistics)this.character.getStats();
         Assertions.assertNotEquals(0, statistics.getTemporaryLife());
         Assertions.assertNotEquals(initialPoints, this.merchant.getLevel().getGameData().getPoints());
     }
 
+    /**
+     * Test if there is no purchase if the character has not enough points.
+     */
     @Test
-    void TestShopEntrance() {
-        final BaseEntrance entrance = new ShopEntrance(this.merchant.getLevel(), Vector2.zero());
-        Assertions.assertEquals(0, this.merchant.getMerch().size());
-        entrance.onCollisionExit(this.character, Vector2.left());
-        Assertions.assertEquals(3, this.merchant.getMerch().size());
-        entrance.onCollisionExit(this.character, Vector2.right());
-        Assertions.assertEquals(0, this.merchant.getMerch().size());
+    void testSellWithoutPoints() {
+        this.merchant.restock();
+        final Level level = this.merchant.getLevel();
+        final GameData gameData = new GameDataImpl(Map.of(CharacterID.CASTER, new Caster(level, Vector2.zero())));
+        level.linkGameData(gameData);
+        final long initialPoints = gameData.getPoints();
+        final int initialQuantity = this.merchant.getMerch().size();
+        this.merchant.getMerch().get(0).interact(this.character);
+        Assertions.assertEquals(initialPoints, gameData.getPoints());
+        Assertions.assertEquals(initialQuantity, this.merchant.getMerch().size());
+    }
 
+    /**
+     * Tests if the computed position of potions is correct.
+     */
+    @Test
+    void testPositionOfPotions() {
+        this.merchant.restock();
+        final Vector2 position1 = this.merchant.getMerch().get(0).getPosition();
+        final Vector2 position2 = this.merchant.getMerch().get(1).getPosition();
+        final Vector2 position3 = this.merchant.getMerch().get(2).getPosition();
+        Assertions.assertEquals(0, Double.compare(Math.abs(this.merchant.getPosition().x() - position1.x()),
+                GameObject.TILE_SIZE * this.merchant.getDistanceFromItems()));
+        Assertions.assertEquals(0, Double.compare(Math.abs(position1.x() - position2.x()),
+                GameObject.TILE_SIZE * this.merchant.getDistanceFromItems()));
+        Assertions.assertEquals(0, Double.compare(Math.abs(position2.x() - position3.x()),
+                GameObject.TILE_SIZE * this.merchant.getDistanceFromItems()));
+    }
+
+    /**
+     * Tests if restock and destock work as expected.
+     */
+    @Test
+    void testShopEntrance() {
+        final ShopEntrance entrance = new ShopEntrance(this.merchant.getLevel(), Vector2.zero());
+        entrance.setMerchant(this.merchant);
+        Assertions.assertEquals(0, this.merchant.getMerch().size());
+        entrance.onCollisionExit(this.character, Vector2.right());
+        Assertions.assertEquals(3, this.merchant.getMerch().size());
+        entrance.onCollisionExit(this.character, Vector2.left());
+        Assertions.assertEquals(0, this.merchant.getMerch().size());
     }
 
 }
