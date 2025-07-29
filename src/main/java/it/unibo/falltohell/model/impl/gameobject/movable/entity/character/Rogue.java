83e43a553e01b849bdbb6a80d96c0d1414de2436
Davide Mancini <davide.mancini10@studio.unibo.it>
2025-07-29 17:02:56 +0200

    Moved timer inside the class as a constructor constant

--- src/main/java/it/unibo/falltohell/model/impl/gameobject/movable/entity/character/Rogue.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobject/movable/entity/character/Rogue.java
@@ -8,6 +8,7 @@ import it.unibo.falltohell.model.api.ability.passive.StatisticPassiveAbility;
 import it.unibo.falltohell.model.api.manager.TimerManager;
 import it.unibo.falltohell.model.api.buff.Buff;
 import it.unibo.falltohell.model.api.factory.AbilityFactory;
+import it.unibo.falltohell.model.api.timer.CustomTimer;
 import it.unibo.falltohell.model.impl.factory.AbilityFactoryImpl;
 import it.unibo.falltohell.model.impl.buff.SpeedBuff;
 import it.unibo.falltohell.model.impl.gameobject.movable.projectile.BaseEnemyProjectile;
@@ -36,6 +37,7 @@ public class Rogue extends BaseCharacter {
 
     private final StatisticPassiveAbility evadeAbility;
     private final SpecialActiveAbility knifeAbility;
+    private final CustomTimer passiveAbilityCooldown;
     private boolean canDoubleJump;
     private boolean canUsePassive;
 
@@ -56,6 +58,7 @@ public class Rogue extends BaseCharacter {
         this.canDoubleJump = false;
         this.canUsePassive = true;
         this.equipWeapon(new Dagger(this));
+        this.passiveAbilityCooldown = new CustomTimerImpl(PASSIVE_COOLDOWN_TIME, () -> this.canUsePassive = true);
         final AbilityFactory factory = new AbilityFactoryImpl();
         this.evadeAbility = factory
             .createPassiveAbility(this, character -> {
@@ -66,14 +69,7 @@ public class Rogue extends BaseCharacter {
                     final String name = "rogue-buff" + speedBuff.hashCode();
                     this.getBuffManager().addBuff(speedBuff, PASSIVE_DURATION, name);
                     this.canUsePassive = false;
-                    if (!tm.searchTimer(passiveCooldownTimerName)) {
-                        tm.addTimer(
-                            passiveCooldownTimerName,
-                            new CustomTimerImpl(PASSIVE_COOLDOWN_TIME, () -> this.canUsePassive = true)
-                        );
-                    } else {
-                        tm.restartTimer(passiveCooldownTimerName);
-                    }
+                    tm.restartIfPresent(passiveCooldownTimerName, this.passiveAbilityCooldown);
                 }
             });
         this.knifeAbility = factory.createSpecialActiveAbility(this);
