6a2dc5132fa490783983dffec9b3e865ef181f9c
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-07-29 23:58:25 +0200

    Added labels

--- src/main/java/it/unibo/falltohell/model/impl/gameobject/interactable/SavePoint.java
+++ src/main/java/it/unibo/falltohell/model/impl/gameobject/interactable/SavePoint.java
@@ -2,7 +2,9 @@ package it.unibo.falltohell.model.impl.gameobject.interactable;
 
 import it.unibo.falltohell.controller.api.SaveFileController;
 import it.unibo.falltohell.controller.impl.SaveFileControllerImpl;
+import it.unibo.falltohell.model.impl.drawable.Label;
 import it.unibo.falltohell.model.impl.gameobject.GameObjectImpl;
+import it.unibo.falltohell.model.impl.timer.CustomTimerImpl;
 import it.unibo.falltohell.util.Priority;
 import it.unibo.falltohell.model.api.level.Level;
 import it.unibo.falltohell.model.api.gameobject.interactable.Interactable;
@@ -16,7 +18,11 @@ import it.unibo.falltohell.util.Vector2;
  */
 public class SavePoint extends GameObjectImpl implements Interactable {
 
+    private static final Vector2 LABEL_POSITION = new Vector2(70.0, 50.0);
+    private static final long LABEL_DURATION = 1500;
     private final SaveFileController saveController;
+    private final Label correctSaveLabel;
+    private final Label incorrectSaveLabel;
 
     /**
      * Initialization of the SavePoint class.
@@ -28,6 +34,10 @@ public class SavePoint extends GameObjectImpl implements Interactable {
         super(level, position, collider);
         this.saveController = new SaveFileControllerImpl();
         this.initDrawable(Priority.VERY_LOW, "save_point.png");
+        this.correctSaveLabel = new Label("Saved the game", LABEL_POSITION, false);
+        this.getLevel().getDrawableRenderableHandler().linkLabel(this.correctSaveLabel);
+        this.incorrectSaveLabel = new Label("Something went wrong", LABEL_POSITION, false);
+        this.getLevel().getDrawableRenderableHandler().linkLabel(this.incorrectSaveLabel);
     }
 
     /**
@@ -35,6 +45,16 @@ public class SavePoint extends GameObjectImpl implements Interactable {
      */
     @Override
     public void interact(final Character character) {
-        this.saveController.save(this.getLevel().getGameData());
+        final boolean correctSave = this.saveController.save(this.getLevel().getGameData());
+        final Label label;
+        if (correctSave) {
+            label = this.correctSaveLabel;
+        } else {
+            label = this.incorrectSaveLabel;
+        }
+        label.setVisible(true);
+        final String timerName = "saving" + label.hashCode();
+        this.getLevel().getTimerManager().restartIfPresent(timerName, new CustomTimerImpl(LABEL_DURATION,
+            () -> label.setVisible(false)));
     }
 }
